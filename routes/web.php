05c81be221a3b35f6c9abeb8f37e563a8fa8a8c6
mregaa <mrega.p77@gmail.com>
2024-12-27 18:53:48 +0700

    livewire (login & register)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('dashboard');
+route::get('/', [HomeController::class,'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+route::get('/redirect', [HomeController::class,'redirect']);
