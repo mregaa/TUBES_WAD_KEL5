7e52aebb7b72ab5da429f714057bb23a84ea9c46
mregaa <mrega.p77@gmail.com>
2024-12-31 20:27:55 +0700

    memanage akun pengguna

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,20 @@
-<x-app-layout>
-    
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+        @include('admin.navbar')
+        <!-- partial -->
+        @include('admin.body')
+
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.scripts')
+    <!-- End custom js for this page -->
+  </body>
+</html>
